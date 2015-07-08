54049aa7c34bf10e3b272d7942bb00857db7b15f
Adriel <adriel@set.tv>
2015-07-07 22:53:23 -0700

    adding the generic parser methods

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='graphql',
+      version='0.1',
+      description='graphql parser',
+      url='https://github.com/AdrielVelazquez/graphql',
+      author='Adriel Velazquez',
+      author_email='adriel@set.tv',
+      license='MIT',
+      packages=['graphql'],
+      zip_safe=False)
