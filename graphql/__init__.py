ff720b7308b524f171822c6ec5a13a855ba056f4
Adriel <adriel@set.tv>
2015-11-16 09:31:45 -0800

    Removing the sqlalchemy componenets since that'll be done in a seperate package

--- graphql/__init__.py
+++ graphql/__init__.py
@@ -1,3 +1,1 @@
 from .grammar_parser import parser
-from .sqlalchemy.class_registry import base_model_registry, model_registry, session_registry, column_type_graph
-from .sqlalchemy.parsed_query import alchemy_data
